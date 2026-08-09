eb563b026c694404e795d373c53ab8d0d21c3c2c
Sebaszc31 <sebaszapatacalderon@gmail.com>
2026-08-09 01:13:40 -0500

    Refactor to use Telegram for weather notifications
    Updated the script to fetch weather data and send Telegram messages instead of sending birthday emails. Removed email-related code and added weather check functionality.

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+TELEGRAM = f"https://api.telegram.org/bot{os.environ.get("TOKEN_TELE")}/sendMessage"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+TELEGRAM_PARAMS = {
+    "chat_id" : os.environ.get("CHAT_ID"),
+    "text" : "It's gonna rain nigga"
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+parameters = {
+    "lat" : 6.15350,
+    "lon" : -75.580694,
+    "appid" : os.environ.get("APPID"),
+    "cnt" : 4
+}
+response = requests.get(url= "https://api.openweathermap.org/data/2.5/forecast", params= parameters)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    response = requests.get(url= TELEGRAM, params= TELEGRAM_PARAMS)
